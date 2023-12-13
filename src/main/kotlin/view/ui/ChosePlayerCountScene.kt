daf763e06cf33e5aa774ba42e58ee25cdfc74376
Thorben <thorben.kiehl@tu-dortmund.de>
2023-12-13 09:48:30 +0100

    ChosePlayerCountScene fertig gestellt

--- src/main/kotlin/view/ui/ChosePlayerCountScene.kt
+++ src/main/kotlin/view/ui/ChosePlayerCountScene.kt
@@ -3,29 +3,41 @@ package view.ui
 import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.core.MenuScene
+import tools.aqua.bgw.util.Font
 
-class ChosePlayerCountScene : MenuScene(1920, 1080) {
+/**
+ * creates a selection menu for the different numbers of players
+ */
+class ChosePlayerCountScene : MenuScene(400, 1080) {
+
+    private val headLabel = Label(
+        width = 300, height = 75,
+        posX = 50, posY = 50,
+        text = "Choose number of players",
+        font = Font(size = 25)
+    )
+
     private val p2Button = Button(
-        width = 100, height = 35,
-        posX = 50, posY = 125,
+        width = 200, height = 50,
+        posX = 100, posY = 150,
         text = "2 Player"
     )
 
     private val p3SharedButton = Button(
-        width = 100, height = 35,
-        posX = 50, posY = 125,
+        width = 200, height = 50,
+        posX = 100, posY = 250,
         text = "3 Player (Shared Gates)"
     )
 
     private val p3OwnButton = Button(
-        width = 100, height = 35,
-        posX = 50, posY = 125,
+        width = 200, height = 50,
+        posX = 100, posY = 350,
         text = "3 Player (Own Gates)"
     )
 
     private val p4Button = Button(
-        width = 100, height = 35,
-        posX = 50, posY = 125,
+        width = 200, height = 50,
+        posX = 100, posY = 450,
         text = "4 Player"
     )
 
@@ -35,6 +47,7 @@ class ChosePlayerCountScene : MenuScene(1920, 1080) {
 
     init {
         opacity = 0.5
-        addComponents(p2Button, p3OwnButton, p3SharedButton, p4Button)
+        addComponents(p2Button, p3OwnButton, p3SharedButton, p4Button,
+            headLabel)
     }
 }
