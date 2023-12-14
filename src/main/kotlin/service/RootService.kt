c6ad53fd0504f5ba998b29cfbce93b076c46534d
shabo <nour.shabou@tu-dortmund.de>
2023-12-14 14:28:37 +0100

    ServiceSchicht erstellt

--- src/main/kotlin/service/RootService.kt
+++ src/main/kotlin/service/RootService.kt
@@ -1,5 +1,8 @@
 package service
 
-// TODO: Implement root service
-class RootService {
+ class RootService { //  val playerActionService=PlayerActionService(this)
+    /**
+     * The currently active game. Can be `null`, if no game has started yet.
+     */
+    // var currentGame : GameState = null
 }
