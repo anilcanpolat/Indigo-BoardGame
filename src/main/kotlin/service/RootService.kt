516557d3dd938abaf887a3032cf67bca8712919e
saadbenboujina <saad.benboujina@gmail.com>
2023-12-14 16:18:58 +0100

    AbstractRefreshingService and refreshable

--- src/main/kotlin/service/RootService.kt
+++ src/main/kotlin/service/RootService.kt
@@ -1,8 +1,21 @@
 package service
 
- class RootService { //  val playerActionService=PlayerActionService(this)
-    /**
-     * The currently active game. Can be `null`, if no game has started yet.
-     */
-    // var currentGame : GameState = null
+
+import edu.udo.cs.sopra.ntf.GameMode
+import entity.Player
+
+/**
+ * Main class of the service layer for the card game. Provides access
+ * to all other service classes and holds the [currentGame] state for these
+ * services to access.
+ */
+class RootService {
+ /**
+  * Startet ein neues Spiel mit den angegebenen Spielern.
+  *
+  * @param playerList Die Liste der Spieler, die am Spiel teilnehmen.
+  */
+ fun startNewGame(playerList: List<Player>,gameMode:GameMode) {
+
+ }
 }
