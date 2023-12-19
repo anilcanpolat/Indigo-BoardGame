91dceec4ceba33040397bd86b4192563786bea60
anilcanpolat <anilcan.polat@tu-dortmund.de>
2023-12-19 16:15:57 +0300

    AIService skeleton is created.

--- src/main/kotlin/service/AIService.kt
+++ src/main/kotlin/service/AIService.kt
@@ -7,5 +7,5 @@ class AIService {
     fun calculateNextMove(): Pair<Tile, Int> {
         TODO("Implement this feature") // with a message
     }
-    //This is a test for Efe, to see if he can see the push and commits
+    //This is a test for Efe, to see if he can see the push and commits.
 }
