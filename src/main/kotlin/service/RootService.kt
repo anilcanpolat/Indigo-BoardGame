e0465f2803bad1de53bd5db1efa1c69bae0ccc7f
fabian.vanrissenbeck <fabian.vanrissenbeck@gmail.com>
2023-12-23 11:15:46 +0100

    fixed incorrect gate configuration

--- src/main/kotlin/service/RootService.kt
+++ src/main/kotlin/service/RootService.kt
@@ -39,7 +39,7 @@ class RootService: AbstractRefreshingService() {
         val tokens = PlayerToken.values().toMutableList()
 
         val playerList = players.map {
-            val token = tokens.removeLast()
+            val token = tokens.removeFirst()
             val tile = tiles.removeFirst()
 
             Player(it.name, it.age, it.type, token, tile)
@@ -56,67 +56,13 @@ class RootService: AbstractRefreshingService() {
         )
 
         val grid = TileGrid(treasureTiles)
-        val gates = gatesForMode(playerList, gameMode)
+        val gates = gameMode.gateConfiguration()
         val board = Board(gates, grid)
 
         currentGame = GameState(playerList[0], board, playerList, tiles)
 
         onAllRefreshables { onGameStart(playerList, gates.toList()) }
     }
-
-     /**
-      * Startet ein neues Spiel mit den angegebenen Spielern.
-      *
-      * @param playerList Die Liste der Spieler, die am Spiel teilnehmen.
-      */
-    fun startNewGame(playerList: List<Player>,gameMode:GameMode) {
-
-    }
-
-    /**
-     * Get the gate configuration for the given game mode.
-     * @param players list of players to get their tokens from
-     * @param mode [GameMode] used to derive the configuration
-     * @return array with 6 items holding two [PlayerToken] objects each
-     */
-    private fun gatesForMode(players: List<Player>, mode: GameMode): Array<Pair<PlayerToken, PlayerToken>> =
-        when (mode) {
-            GameMode.TWO_PLAYERS -> {
-                val fst = players[0].playerToken
-                val snd = players[1].playerToken
-
-                val fstPair = Pair(fst, fst)
-                val sndPair = Pair(snd, snd)
-
-                arrayOf(fstPair, sndPair, fstPair, sndPair, fstPair, sndPair)
-            }
-            GameMode.THREE_PLAYERS -> {
-                val fst = players[0].playerToken
-                val snd = players[1].playerToken
-                val thd = players[2].playerToken
-
-                val fstPair = Pair(fst, fst)
-                val sndPair = Pair(snd, snd)
-                val thdPair = Pair(thd, thd)
-
-                arrayOf(fstPair, sndPair, thdPair, fstPair, sndPair, thdPair)
-            }
-            GameMode.THREE_PLAYERS_SHARED_GATES -> {
-                val fst = players[0].playerToken
-                val snd = players[1].playerToken
-                val thd = players[2].playerToken
-
-                arrayOf(Pair(fst, fst), Pair(fst, snd), Pair(thd, thd), Pair(thd, fst), Pair(snd, snd), Pair(snd, thd))
-            }
-            GameMode.FOUR_PLAYERS -> {
-                val fst = players[0].playerToken
-                val snd = players[1].playerToken
-                val thd = players[2].playerToken
-                val fth = players[3].playerToken
-
-                arrayOf(Pair(fst, snd), Pair(snd, thd), Pair(fst, fth), Pair(fth, snd), Pair(thd, fst), Pair(thd, fth))
-            }
-        }
 
     /**
      * Get the number of players required for the given [GameMode]
