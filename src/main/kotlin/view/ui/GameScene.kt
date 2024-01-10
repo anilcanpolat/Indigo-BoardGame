d6d4866cfa6979456c92ab7b9f009bd03843d4ed
Dogan Can Demir <dogan.demir@tu-dortmund.de>
2024-01-10 23:42:43 +0100

    PlayerScore Panes created

--- src/main/kotlin/view/ui/GameScene.kt
+++ src/main/kotlin/view/ui/GameScene.kt
@@ -6,10 +6,13 @@ import service.Refreshable
 import tools.aqua.bgw.components.ComponentView
 import tools.aqua.bgw.components.container.HexagonGrid
 import tools.aqua.bgw.components.gamecomponentviews.HexagonView
+import tools.aqua.bgw.components.layoutviews.GridPane
 import tools.aqua.bgw.components.layoutviews.Pane
 import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.components.uicomponents.Label
+import tools.aqua.bgw.core.Alignment
 import tools.aqua.bgw.core.BoardGameScene
+import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
 import tools.aqua.bgw.visual.CompoundVisual
 import tools.aqua.bgw.visual.ImageVisual
@@ -55,6 +58,11 @@ class GameScene : BoardGameScene(1920, 1080),Refreshable {
             ImageVisual(path = "redo.png"))
     }
 
+    private val routeStack = HexagonView(
+        posX = 100, posY = 500, size =65, visual = CompoundVisual(
+            ColorVisual(250,240,202),ImageVisual(path = "BacksideTile.png"))
+    ).apply { rotate(30) }
+
     private val rotateLeftButton = Button(
         width = 130, height = 50,
         posX = 100, posY = 900, visual = ColorVisual.GRAY, text = "Rotate Left"
@@ -77,17 +85,49 @@ class GameScene : BoardGameScene(1920, 1080),Refreshable {
         posX = 100, posY = 750, size =65, visual = ImageVisual(path = "longCurveTile.png"),
     ).apply { rotate(30) }
 
-    private val routeStack = HexagonView(
-        posX = 100, posY = 500, size =65, visual = CompoundVisual(
-            ColorVisual(250,240,202),ImageVisual(path = "BacksideTile.png"))
-    )
 
-    //It is for layout of the playerName and score boxes
-    private val playerLayout = Pane<ComponentView>(
-        posX = 1500, posY = 50 , width = 400, height = 1000
+
+    //It is for layout of the playerName and score
+    private val player1Pane = Pane<ComponentView>(
+        width = 300, height = 200
+    ).apply { visual = ColorVisual(ColorEnum.Papaya.toRgbValue()) }
+
+    private val player1Text = Label(text = "Player1", posX = 10 ,
+        font = Font(size = 30), width = 200,
+        alignment = Alignment.CENTER_LEFT )
+    private val player1Color = Label(visual = ImageVisual(path = "BigPlayerColorFour.png"),
+        width = 70, height = 70, posX = 200, posY = 20
     )
+    private val player1ScoreText = Label(text = "Score:", posX = 10, posY = 50, font = Font(size=30),
+        width = 200, alignment = Alignment.CENTER_LEFT)
+    private val player1Score = Label(text = "0",
+        posX = 100, posY = 50, font = Font(size = 30), alignment = Alignment.CENTER_LEFT)
+    private val player1Tile = HexagonView(
+        posX = 10, posY = 70,
+        visual = CompoundVisual(ColorVisual(250,240,202),ImageVisual(path = "BacksideTile.png")),
+        size = 65).apply { rotate(30) }
+
+
+    private val player2Pane = Pane<ComponentView>(
+        width = 300, height = 200
+    ).apply { visual = ColorVisual(ColorEnum.Papaya.toRgbValue()) }
+    private val player3Pane = Pane<ComponentView>(
+        width = 300, height = 200
+    ).apply { visual = ColorVisual(ColorEnum.Papaya.toRgbValue()) }
+    private val player4Pane = Pane<ComponentView>(
+       width = 300, height = 200
+    ).apply { visual = ColorVisual(ColorEnum.Papaya.toRgbValue()) }
+    private val gridPane = GridPane<ComponentView>(posX = 1750, posY = 500 , columns = 1, rows = 4, spacing = 20)
+
 
     init {
+        player1Pane.addAll(player1Text, player1Color, player1ScoreText, player1Score,player1Tile)
+        gridPane[0,0]= player1Pane
+        gridPane[0,1] = player2Pane
+        gridPane[0,2] = player3Pane
+        gridPane[0,3] = player4Pane
+
+
         //There should not be a coordinate like [4,1], [-4,-1] or [-4,-4] and others
         for (row in -4..4) {
             for (col in -4..4) {
@@ -102,16 +142,12 @@ class GameScene : BoardGameScene(1920, 1080),Refreshable {
         placeTiles()
         background = ColorVisual.LIGHT_GRAY
         addComponents(saveButton, quitButton,redoButton,undoButton,
-            hexagonGrid, rotateLeftButton,rotateRightButton, playersTile, routeStack,playerLayout)
+            hexagonGrid, rotateLeftButton,rotateRightButton, playersTile, routeStack,gridPane)
     }
 
 
      override fun onGameStart(players: List<Player>, gates: List<Pair<PlayerToken, PlayerToken>>) {
-         val playerNames = mutableListOf<Label>()
-         for (i in players){
-             val text = Label(text = i.name)
-             playerNames.add(text)
-         }
+
 
     }
 
