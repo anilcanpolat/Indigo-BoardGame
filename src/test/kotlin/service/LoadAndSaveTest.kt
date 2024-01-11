13e4d9efc5170665ee5b1d624f32daad77d7b090
fabian.vanrissenbeck <fabian.vanrissenbeck@gmail.com>
2024-01-11 16:21:33 +0100

    added test asserting that serializing more complex GameState instances is done correctly

--- src/test/kotlin/service/LoadAndSaveTest.kt
+++ src/test/kotlin/service/LoadAndSaveTest.kt
@@ -1,47 +1,102 @@
-    package service
+package service
 
-    import entity.*
-    import org.junit.jupiter.api.Assertions.*
-    import org.junit.jupiter.api.Test
-    import java.io.File
+import entity.GameMode
+import entity.GameState
+import entity.PlayerConfig
+import entity.PlayerType
+import kotlin.test.*
+import java.io.File
 
-    class LoadAndSaveTest {
-        /** for save test , we Create an instance of RootService
-         *  Start a new game
-         * Save the game to a  file
-         * Verify that the file exists
-         * load this file
-         * Than Compare the loadedGame with the currentGame in rootService
-         */
+/**
+ * Test cases for [RootService.save] and [RootService.load].
+ */
+class LoadAndSaveTest {
+    /**
+     * Make sure that serialization and deserialization of the full [GameState]
+     * with set instances of [GameState.previousState] and [GameState.nextState]
+     * is done correctly.
+     *
+     * The [GameState] is mutated to appear as if two moves have already been executed
+     * and [RootService.undo] has been called. So [RootService.currentGame] should point
+     * to the second most recent [GameState] instance.
+     *
+     * After that the [RootService.currentGame] is serialized and written to a temporary
+     * file. The file will be deleted after completing the test. The temporary file
+     * is then reloaded by a different [RootService] instance. Both contained [GameState]
+     * objects are compared for equality, including the previous and next states.
+     */
+    @Test
+    fun saveAndLoadTest() {
+        val rootService = RootService()
 
-        @Test
-        fun saveAndLoadTest() {
-            val rootService = RootService()
-            val players = listOf(
-                PlayerConfig("Player1", 25, PlayerType.PERSON),
-                PlayerConfig("Player2", 30, PlayerType.PERSON)
-            )
-            rootService.startGame(players, GameMode.TWO_PLAYERS)
-            val tempFilePath = "Indigo.json"
-            rootService.save(tempFilePath)
-            val savedFile = File(tempFilePath)
-            assertTrue(savedFile.exists())
+        val players = listOf(
+            PlayerConfig("Alice", -1, PlayerType.PERSON),
+            PlayerConfig("Bob", -1, PlayerType.PERSON)
+        )
 
-            val loadedGame: GameState? = rootService.load(tempFilePath)
-            assertEquals(loadedGame, rootService.currentGame)
-        }
+        rootService.startGame(players, GameMode.TWO_PLAYERS)
 
-        /**loadEmptyFileTest will :
-         *   Save an empty file first
-         *   load this file
-         *   Verify that the loaded game state is null
-         */
-        @Test
-        fun loadEmptyFileTest() {
-            val rootService = RootService()
-            val tempFilePath = "Indigo.json"
-            File(tempFilePath).writeText("")
-            val loadedGame: GameState? = rootService.load(tempFilePath)
-            assertNull(loadedGame)
-        }
+        val currentGame = checkNotNull(rootService.currentGame)
+
+        // Create two fake moves
+        val nextState = GameState(
+            currentGame.currentPlayer,
+            currentGame.board,
+            currentGame.players,
+            currentGame.drawPile
+        )
+
+        val nextNextState = GameState(
+            currentGame.currentPlayer,
+            currentGame.board,
+            currentGame.players,
+            currentGame.drawPile
+        )
+
+        currentGame.nextState = nextState
+        nextState.nextState = nextNextState
+        nextState.previousState = currentGame
+        nextNextState.previousState = nextState
+
+        rootService.redo() // jump to nextState
+
+        val tmpFile = File.createTempFile("savestate", ".json")
+        tmpFile.deleteOnExit()
+
+        rootService.save(tmpFile)
+
+        val secondRoot = RootService()
+        secondRoot.load(tmpFile)
+
+        val testGame = checkNotNull(secondRoot.currentGame) { "state not restored" }
+        val curGame = checkNotNull(rootService.currentGame)
+
+        assertTrue(gameStateEqual(testGame, curGame), "restored gamestate does not equal the previous one")
+
+        assertTrue(
+            gameStateEqual(
+                checkNotNull(testGame.previousState) { "previous state was not restored" },
+                checkNotNull(curGame.previousState)
+            ),
+            "previous gamestate does not equal the previous previous gamestate one"
+        )
+
+        assertTrue(
+            gameStateEqual(
+                checkNotNull(testGame.nextState) { "next state was not restored" },
+                checkNotNull(curGame.nextState)
+            ),
+            "next gamestate does not equal the previous next gamestate one"
+        )
     }
+
+    /**
+     * Check whether two [GameState] instances are equal, without considering
+     * the equality of the [GameState.nextState] and [GameState.previousState] fields.
+     */
+    private fun gameStateEqual(fst: GameState, snd: GameState): Boolean =
+        fst.currentPlayer == snd.currentPlayer &&
+                fst.players == snd.players &&
+                fst.drawPile == snd.drawPile &&
+                fst.board == snd.board
+}
