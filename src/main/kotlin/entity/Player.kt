e87e838ddd5879e63eff3b77460510eeeda98872
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2024-01-18 16:38:40 +0100

    added useRandomAI and aiDelay properties

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -5,14 +5,15 @@ import kotlinx.serialization.Serializable
 /**
  * Class wrapping the state associated with a single player.
  */
-
 data class Player(
     val name: String,
     val age: Int,
     val playerType: PlayerType,
     val playerToken: PlayerToken,
     var currentTile: Tile? = null,
-    val collectedGems: MutableList<Gem> = mutableListOf()
+    val collectedGems: MutableList<Gem> = mutableListOf(),
+    val useRandomAI: Boolean = false,
+    val aiDelay: Int = 0
 ) {
     /** create a deepCopy of the current [Player] instance */
     fun deepCopy(): Player =
