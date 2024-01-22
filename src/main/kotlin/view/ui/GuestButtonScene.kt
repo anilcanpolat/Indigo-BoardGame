283d1264569ba413847ec57cc9b2c6f3b4836c3c
Dogan Can Demir <dogan.demir@tu-dortmund.de>
2024-01-22 14:12:45 +0100

    GuestButtonScene

--- src/main/kotlin/view/ui/GuestButtonScene.kt
+++ src/main/kotlin/view/ui/GuestButtonScene.kt
@@ -1,5 +1,9 @@
 package view.ui
 
+import entity.Player
+import entity.PlayerConfig
+import entity.PlayerType
+import service.NetworkService
 import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.components.uicomponents.TextField
@@ -9,7 +13,7 @@ import tools.aqua.bgw.visual.CompoundVisual
 import tools.aqua.bgw.visual.ImageVisual
 import java.awt.Color
 
-class GuestButtonScene : MenuScene(1920, 1080,
+class GuestButtonScene(networkService: NetworkService) : MenuScene(1920, 1080,
     background = ImageVisual("cecihoney-background-desert-full.jpg")){
 
     val returnGuestButton = Button(
@@ -63,26 +67,46 @@ class GuestButtonScene : MenuScene(1920, 1080,
     }
 
     private val kiSpeedGuestA = Label(
-        posX = 770, posY = 585,
+        posX = 770, posY = 650,
         width = 75, height = 50,
         text = "Ki-Speed: "
     )
 
     private val kiSpeedGuestB = Label(
-        posX = 895, posY = 585,
+        posX = 895, posY = 650,
         width = 50, height = 50,
         text = "ms "
     )
 
     private val kiSpeedGuestText = TextField(
-        posX = 855, posY = 595,
+        posX = 855, posY = 650,
         width = 40, height = 30,
         text = "250"
     )
+    val joinButton = Button(
+        posX = 860, 700,
+        200, 50,
+        text = "Join "
+    ).apply { visual = ColorVisual(ColorEnum.Wheat.toRgbValue())
+    onMouseClicked ={
+        val player  = configureGuestPlayer()
+        networkService.joinGame(guestIdField.text, player)
+
+    }}
+
+    private val guestIdField = TextField(
+        posX = 885, 600,
+        150, 30,
+        prompt = "Enter id: "
+    ).apply { visual = ColorVisual(ColorEnum.Wheat.toRgbValue())
+        onKeyTyped = {
+        }
+    }
 
 
-    private fun configureGuestPlayer(){
 
+    private fun configureGuestPlayer() : PlayerConfig{
+        return PlayerConfig(playerATextBox.text,0,PlayerType.REMOTE)
     }
 
     init {
@@ -91,7 +115,7 @@ class GuestButtonScene : MenuScene(1920, 1080,
             playerATextBox,
             kiButtonA, kiButtonB,
             kiSpeedGuestA, kiSpeedGuestB,
-            kiSpeedGuestText
+            kiSpeedGuestText,joinButton, guestIdField
         )
     }
 }
